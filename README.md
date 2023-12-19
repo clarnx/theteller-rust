93908ee3502270707cc69e4d0307daea4bbdbaed
clarnx <clarenceonline@outlook.com>
2023-12-19 18:06:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TheTeller Rust Library
+
+TheTeller Rust library provides convenient access to the TheTeller API from applications written in Rust.
---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+
+use base64::encode;
+use reqwest::{Client, RequestBuilder};
+
+#[derive(Debug, Clone, Copy)]
+pub enum RequestMethod {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+}
+
+use RequestMethod::*;
+
+pub struct TheTellerClient {
+    url: String,
+    request_headers: HashMap<&'static str, String>,
+    request_payload: HashMap<&'static str, String>,
+}
+
+impl TheTellerClient {
+    /// Create a new account with the given secret key.
+    pub fn new(secret_key: &str) -> Self {
+        let base_64_encoded_secret = encode(secret_key);
+
+        let mut request_headers = HashMap::new();
+        let mut request_payload = HashMap::new();
+
+        request_headers.insert("Content-Type", "application/json".to_string());
+        request_headers.insert("Authorization", format!("Basic {}", base_64_encoded_secret));
+        request_headers.insert("Cache-Control", "no-cache".to_string());
+
+        Self {
+            url: "".to_string(),
+            request_headers,
+            request_payload,
+        }
+    }
+
+    pub fn create_request(&mut self, method: RequestMethod, url: &str) -> RequestBuilder {
+        self.url = url.to_string();
+
+        let client = Client::new();
+
+        match method {
+            GET => client.request(reqwest::Method::GET, url),
+            POST => client.request(reqwest::Method::POST, url),
+            PUT => client.request(reqwest::Method::PUT, url),
+            PATCH => client.request(reqwest::Method::PATCH, url),
+            DELETE => client.request(reqwest::Method::DELETE, url),
+        }
+    }
+}
+
+pub mod checkout {
+    pub mod initialize {
+        pub use crate::checkout::initialize;
+    }
+}
