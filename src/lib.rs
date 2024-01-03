ef0ddf6f3ef3e2985c2714c7de2afad8c7606024
clarnx <clarenceonline@outlook.com>
2024-01-03 07:31:48 +0000

    added trasaction, payments, and transfer modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod traits {
-    pub mod create_request_payload;
+    pub mod custom_client_functions;
 }
 
 pub mod client {
@@ -8,4 +8,21 @@ pub mod client {
 
 pub mod resources {
     pub mod checkout;
+    pub mod transaction;
+    pub mod transfer;
+    pub mod payments;
+}
+
+pub mod utils {
+    pub mod amount_to_minor_unit;
+    pub mod amount_to_string;
+    pub mod generate_transaction_id;
+    pub mod generate_request_id;
+}
+
+pub mod constants {
+    pub mod endpoints;
+    pub mod account_issuers;
+    pub mod bank_codes;
+    pub mod processing_codes;
 }
