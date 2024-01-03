0e12892e9fb083223c7d9ca0dfe3b5fd720c1fa5
clarnx <clarenceonline@outlook.com>
2024-01-03 07:34:28 +0000

    add implementation for ReqwestClient to set headers

--- src/client/async.rs
+++ src/client/async.rs
@@ -1,16 +1,31 @@
-use std::collections::HashMap;
-
 use base64::{engine::general_purpose, Engine};
 use reqwest::{
     header::{self, HeaderMap},
-    Client as ReqwestClient, RequestBuilder,
+    Client as ReqwestClient,
 };
+
+use crate::traits::custom_client_functions::CustomClientFunctions;
 
 pub struct Client;
 
+impl CustomClientFunctions for ReqwestClient {
+    fn set_headers(&self, request_headers: HeaderMap) -> ReqwestClient {
+        // Create a builder for the client
+        let client_builder = ReqwestClient::builder();
+
+        // Add headers to the builder
+        let configured_client_builder = client_builder.default_headers(request_headers);
+
+        // Build the client
+        let client = configured_client_builder.build().unwrap();
+
+        client
+    }
+}
+
 impl<'a> Client {
     /// Create a new account with the given secret key.
-    pub fn new(secret_key: &'a str) -> RequestBuilder {
+    pub fn new(secret_key: &'a str) -> ReqwestClient {
         let base_64_encoded_secret = general_purpose::STANDARD.encode(secret_key);
         let authorization = format!("Basic {}", base_64_encoded_secret);
 
@@ -35,11 +50,26 @@ impl<'a> Client {
         return Self::configure(request_headers);
     }
 
-    fn configure(request_headers: HeaderMap) -> RequestBuilder {
-        let request_builder = ReqwestClient::new().head("").headers(request_headers);
-
-        return request_builder;
+    pub fn create() -> ReqwestClient {
+        ReqwestClient::new()
     }
+
+    fn configure(request_headers: HeaderMap) -> ReqwestClient {
+        // Create a builder for the client
+        let client_builder = ReqwestClient::builder();
+
+        // Add headers to the builder
+        let configured_client_builder = client_builder.default_headers(request_headers);
+
+        // Build the client
+        let client = configured_client_builder.build().unwrap();
+
+        client
+    }
+
+    // pub fn set_headers(&self, request_headers: HeaderMap) -> ReqwestClient {
+    //     return Self::configure(request_headers);
+    // }
 
     // pub fn create_request(&mut self, method: RequestMethod, url: &str) -> RequestBuilder {
     //     self.url = url.to_string();
